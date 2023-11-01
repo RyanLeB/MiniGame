9383d2b2b0d3833801a1b08e1aa0c0d36be83658
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-11-01 13:51:01 -0300

    First Commit

---
+++ MiniGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGame
+{
+    internal class Program
+    {
+
+        static string[] player;
+        
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
